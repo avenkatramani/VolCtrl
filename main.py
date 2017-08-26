a35847f7648037d17270daf787aa1fc2d1b080d9
Aditya Venkatramani <avenkatramani@g.harvard.edu>
2017-08-25 20:00:26 -0400

    Added kivy project too

---
+++ main.py
@@ -0,0 +1,30 @@
+from kivy.app import App
+from kivy.uix.slider import Slider
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host ="192.168.0.137"
+port =1234
+
+class CircleApp(App):
+	def build(self):
+		layout1 = BoxLayout(orientation='horizontal')
+		self.e1 = Slider(min=0, max = 100, value=0,orientation='vertical')
+		self.txt = Label()
+		self.txt.text = 'Volume : '+ '0'
+		layout1.add_widget(self.e1)
+		layout1.add_widget(self.txt)
+		self.e1.bind(value=self.updateValue)
+		return layout1
+	def updateValue(self, nstance,value):
+		self.txt.text = 'Volume : '+str(value)
+		try:
+			s.connect((host,port))
+			send(str(value).encode()) 
+			s.close()
+			print('Sent Volume')
+		except:
+			pass
+			
+CircleApp().run()
