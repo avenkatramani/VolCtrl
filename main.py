9512e33dcb4f95fac82bdada08eae7414c8b9c82
Aditya <avvenkat@mit.edu>
2017-08-25 21:40:49 -0400

    Fixed multi device sync

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from kivy.app import App
 from kivy.uix.slider import Slider
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-import socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+import socket 
 host ="192.168.0.137"
 port =1234
 
 class CircleApp(App):
+	def connect(self):
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.s.connect((host,port))
+		
 	def build(self):
 		layout1 = BoxLayout(orientation='horizontal')
 		self.e1 = Slider(min=0, max = 100, step = 1, value=0,orientation='vertical')
@@ -17,14 +20,24 @@ class CircleApp(App):
 		layout1.add_widget(self.txt)
 		self.e1.bind(value=self.updateValue)
 		return layout1
+		
 	def updateValue(self, nstance,value):
-		self.txt.text = 'Volume : '+str(int(value))
+		self.txt.text = 'Volume : '+str(value)
 		try:
-			s.connect((host,port))
-			send(str(int(value)).encode()) 
-			s.close()
-			print('Sent Volume')
+			self.s.send(str(value))
 		except:
 			pass
 			
-CircleApp().run()
+	def recieveData(self):
+		while True:
+			try:
+				data = self.s.recv(1024)
+				self.txt.text = 'Volume : '+str(data)
+				self.e1.value = int(data)
+			except:
+				pass
+
+c = CircleApp()
+c.connect()
+start_new_thread(c.recieveData,())
+c.run()
